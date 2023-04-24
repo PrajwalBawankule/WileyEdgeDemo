7ff478a93dc8f69db092727714b64cc45cc7766e
prajwal <prajwalbawankule19@gmail.com>
2023-04-24 10:18:46 +0530

    Interface Demo

--- JavaBasicsDay2/wiley_core_java/src/Wiley_Core_Java/AbstractionClass.java
+++ JavaBasicsDay2/wiley_core_java/src/Wiley_Core_Java/AbstractionClass.java
@@ -3,16 +3,41 @@ package Wiley_Core_Java;
 abstract class Test1{
 	public abstract void m1();
 	
+	{  //Instance Block
+		System.out.println("Test1 instance block");
+	}
+	static{  //Static Block executes only once 
+		System.out.println("Test1 static block");
+	}
 	public void m2() {
 		System.out.println("Test1 m2 method");
+	}
+	
+	public static void main(String[] args) {
+		System.out.println("Test1 main method from abstract class");
+	}
+	
+	public Test1() {
+		System.out.println("Test 1 constructor");
 	}
 }
 
 class Test2 extends Test1{
 
+	{
+		System.out.println("Test2 instance block");
+	}
+	
+	static{  //Static Block executes only once 
+		System.out.println("Test2 static block");
+	}
+	
 	public void m1() {
 		System.out.println("Test2 m1 method");
-		
+	}
+	
+	public Test2() {
+		System.out.println("Test 2 constructor");
 	}
 	
 }
@@ -23,5 +48,6 @@ public class AbstractionClass {
 	   Test2 t2 = new Test2();
 	   t2.m1();
 	   t2.m2();
+	   Test1.main(args);
 	}
 }
