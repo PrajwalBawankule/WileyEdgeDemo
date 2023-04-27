7aaa855267bc1da18ec7b2b7aeccff99813b80f0
prajwal <prajwalbawankule19@gmail.com>
2023-04-27 11:48:04 +0530

    pattern examples regex'

--- Java_Core/wiley_core_java/src/Patterns/PatternMatch.java
+++ Java_Core/wiley_core_java/src/Patterns/PatternMatch.java
@@ -8,11 +8,14 @@ public class PatternMatch {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Pattern pattern = Pattern.compile("wiley");
-		Matcher matcher = pattern.matcher("wiley");
+		Pattern pattern = Pattern.compile("wiley \\d\\d\\d");
+		Matcher matcher = pattern.matcher("wiley 356 4");
 		
 		boolean f = matcher.find();
-		System.out.println(f);
+		boolean m = matcher.matches();
+		
+		System.out.println(f); //matches if the characters are present in string
+		System.out.println(m); //matches exact pattern
 	}
 
 }
